e109fc14a73a1d7b07d091cd4f4b9dbc89578d67
razumolla <razumolla75@gmail.com>
2024-11-17 17:46:30 +0600

    model and signup api

--- src/app.js
+++ src/app.js
@@ -3,6 +3,27 @@ const connectDB = require('./config/database');
 const app = express();
 const PORT = 3000;
 
+const User = require('./models/user');
+
+app.post('/signup', async (req, res) => {
+
+  //creating a new instance of user model
+  const user = new User({
+    firstName: "Hadiz",
+    lastName: "Rahman",
+    emailId: "hafiz@gmail.com",
+    password: "123456",
+    age: 25,
+    gender: "Male",
+  });
+  try {
+    await user.save();
+    res.send("User Created Successfully");
+  } catch (err) {
+    res.status(400).send("User Creation Failed", err.message);
+  }
+});
+
 connectDB()
   .then(() => {
     console.log("Database Connected Successfully");
