40be224900a5e047a289a2181f331ccc565343f3
razumolla <rajuahmed9344@gmail.com>
2024-11-26 23:59:53 +0600

    user all connections

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const userRouter = express.Router();
-const User = require("../models/user");
 const { userAuth } = require('../middlewares/auth');
 const ConnectionRequest = require('../models/connectionRequest');
+const USER_SAFE_DATA = "firstName lastName photoUrl age gender about skills";
 
 // @desc      Get all the connection requests received by the user
 // @route     GET /api/v1/user/requests/received
@@ -14,13 +14,49 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
       toUserId: loggedInUser._id,
       status: "interested"
     })
-      .populate("fromUserId", ["firstName", "lastName"])
-      .populate("toUserId", ["firstName", "lastName"]);
+      .populate("fromUserId", USER_SAFE_DATA)
+      .populate("toUserId", USER_SAFE_DATA);
     // .populate("toUserId"); // Over fetching data - that is unnecessary
 
     res.status(200).json({
       message: "Data retrieved successfully",
       data: connectionRequests
+    });
+  } catch (err) {
+    res.status(400).send("ERROR:" + err.message);
+  }
+});
+
+// @desc       Find all my connection people
+// route      Get /user/connections
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    // loggedINUser can be fromUserId or toUserId
+    // so we need to check if the loggedInUser is fromUserId or toUserId
+    // and then populate the user
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [
+        { fromUserId: loggedInUser._id, status: "accepted" },
+        { toUserId: loggedInUser._id, status: "accepted" }
+      ]
+    })
+      .populate("fromUserId", USER_SAFE_DATA)
+      .populate("toUserId", USER_SAFE_DATA);
+
+    // Extract connections based on whether the logged-in user is the sender or receiver
+    const connections = connectionRequests.map(row => {
+      if (String(row.fromUserId._id) === String(loggedInUser._id)) {
+        return row.toUserId; // If loggedInUser is (fromUserId), return the (toUserId)
+      } else {
+        return row.fromUserId;  // If loggedInUser is (toUserId), return oposite (fromUserId)
+      }
+    });
+
+    res.status(200).json({
+      message: "Data retrieved successfully",
+      data: connections
     });
   } catch (err) {
     res.status(400).send("ERROR:" + err.message);
